cd5f6e70047e5e693da6163b2f3522c30f4e9060
tebibux <tebibusolomon79@gmail.com>
2022-08-27 02:09:17 +0300

    wining senarios

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,12 +1,15 @@
+// global array 
+let rosci = ['ROCK', 'PAPER', 'SCISSOR']
+
+// generate a random number from 1 - 3
+
 function getComputerChoice() {
 	let numb = Math.floor((Math.random() * 3))
-	let rosci = ['ROCK', 'PAPER', 'SCISSOR']
 	if (numb === 1) {
 		return rosci[0];
 	} else if (numb === 2) {
 		return rosci[1];
-	}
-	else {
+	} else {
 		return rosci[2];
 
 	}
@@ -17,10 +20,25 @@ function playRound() {
 	const playerSelection = (prompt('Enter one of "Rock", "paper" "scissor":')).toUpperCase();
 	// get the compter input
 	const computerSelection = getComputerChoice();
+	// winnig condion [0:rock, 1:paper, 2:scissor]
+	// if the same draw
 	if (playerSelection === computerSelection) {
 		console.log('One of you must loos! play again!');
-	} else {
-		return console.log(playerSelection, computerSelection)
+	}
+
+	else {
+		// scissors wins paper 
+		if (playerSelection === rosci[2] && computerSelection === rosci[1]) {
+			console.log('Scissor')
+		}
+		// paper wins rock 
+		else if (playerSelection === rosci[1] && computerSelection === rosci[0]) {
+			console.log('Paper')
+		}
+		// rock wins scissor
+		else{
+			console.log('Rock')
+		}
 	}
 }
 function game() {
