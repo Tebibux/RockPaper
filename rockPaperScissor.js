c2afe8e9f154f8af4815e7e47ffc533213e60dcc
tebibux <tebibusolomon79@gmail.com>
2022-08-27 02:55:37 +0300

    conditional done

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -26,37 +26,61 @@ function playRound() {
 	// winnig condion [0:rock, 1:paper, 2:scissor]
 	// if the same draw
 	if (playerSelection === computerSelection) {
-		return player;
+		// return player;
+		return 0;
 	}
 	// scissors wins paper // player wins computer
 	else if (playerSelection === rosci[2] && computerSelection === rosci[1]) {
-		return player;
+		// return player;
+		return 1;
 	}
 	// paper wins rock // player wins computer
 	else if (playerSelection === rosci[1] && computerSelection === rosci[0]) {
-		return player;
+		// return player;
+		return 1;
 	}
 	// rock wins scissor// player wins computer
 	else if (playerSelection === rosci[0] && computerSelection === rosci[2]) {
-		return player;
+		// return player;
+		return 1;
 	}
 	// the above couldn't meet the compuer wins
 	else {
-		return computer;
+		// return computer;
+		return 2;
 	}
 }
 
 
 function game() {
-	// counting the score from the start
+	// counting the score from the start and bTs(Both Team to Score)
 	let plaWin = 0
 	let compWin = 0
+	let bTs = 0
 	for (let i = 0; i < 5; i++) {
-		if (player === playRound()) {
-			console.log(player);
+		roundPlay = playRound()
+		// add the value depending on return of play round
+		// add 1 each time human win
+		if (roundPlay === 1) {
+			plaWin += 1;
 		}
+		// add 1 each time computer win
+		else if (roundPlay === 2) {
+			compWin += 1;
+		}
+		// add 1 each time score none
 		else {
-			console.log(computer);
+			bTs += 1;
 		}
 	}
+	// consol log winner of the game
+	// human win
+	if (plaWin > compWin) {
+		console.log(`Human won by ${plaWin} to ${compWin}`);
+	}
+	// computer win
+	else {
+		console.log(`Computer won by ${compWin} to ${plaWin}`);
+	}
+	console.log(`Equal result is ${bTs}`)
 }
